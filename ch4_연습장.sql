7c5f7adca7d6c9e93ef827996cb6e47c7a9e59dd
tlsdn741621 <tlsdn741621@gmail.com>
2025-05-19 10:34:40 +0900

    third commit

--- ch4_연습장.sql
+++ ch4_연습장.sql
@@ -68,7 +68,7 @@ SELECT ENAME AS "사원명", SAL * 12 AS "연봉" FROM EMP;
 
 --퀴즈1
 -- EMP 테이블에서 사원 이름에 '각자 정하고 싶은 이름' 별칭을 부여해서 출력해보기
-SELECT EMPNO AS "사원번호", ENAME AS "사원명", JOB AS "직무", MGR AS "관리번호",
+SELECT EMPNO AS "사원번호", ENAME AS "사원명", JOB AS "직무", MGR AS "직속상관사원번호",
 HIREDATE AS "입사일", SAL AS "급여", COMM AS "보너스", DEPTNO AS "부서번호" FROM EMP;
 --퀴즈2
 -- EMP 테이블에서 급여(SAL)를 연봉으로 계산해서 출력해보기 한번더
@@ -81,4 +81,37 @@ SELECT ENAME AS "사원이름", JOB AS "직무" FROM EMP;
 -- 출력 별칭은 "총 급여"로 지정해서 출력
 -- 특정 옵션 함수 NVL(COMM,0):COMM 있으면, COMM 값으로 출력
 -- 특정 옵션 함수 NVL(COMM,0):COMM 없으면, 0 값으로 출력
-SELECT ENAME AS "사원명",SAL AS "급여",COMM AS "커미션",SAL * 12 AS "연봉", SAL * 12 + NVL(COMM,0) AS "총 급여" FROM EMP;
+-- NVL 의미 : N(NULL) 값이 없음, V(value) : 값, L(LOGIC) : 논리 null 값을 처리하기 위한 로직
+SELECT ENAME AS "사원명",SAL AS "기본급",COMM AS "상여금",SAL * 12 AS "연봉", SAL * 12 + NVL(COMM,0) AS "총 연봉" FROM EMP;
+
+--ORDER BY 컬럼명 DESC,ASC(기본)
+
+-- 복합 컬럼 이용
+-- 정렬 기준, 첫 번째 기준으로 정렬하고
+-- 동일한 값이 있을 때, 두번째 기준 정렬
+
+-- 부서 내에서 급여를 높은 순서 정렬
+SELECT * FROM EMP ORDER BY DEPTNO ASC, SAL DESC;
+
+-- 열 인덱스로 정렬하기
+-- ENAME(1),JOB(2),SAL(3)
+SELECT ENAME, JOB, SAL FROM EMP ORDER BY 3 DESC;
+
+-- 시간은 , 최신일 값으로 하면 큰값
+-- 과거 일수록 값으로 하면 작은값
+-- EMP 테이블에서 모든 사원의 입사일 기준으로
+-- 최신순으로 정렬 해보기
+SELECT * FROM EMP ORDER BY HIREDATE DESC;
+
+-- 퀴즈1 
+-- 커미션이 높은 순으로, 급여가 낮은 순으로 정렬 출력
+-- 특정 컬럼 언급 없으면 모든 컬럼 출력
+SELECT * FROM EMP ORDER BY COMM DESC, SAL ASC;
+-- 퀴즈2
+-- EMP 테이블에서 이름, 부서번호, 급여를 출력하되
+-- 급여가 높은 순으로 정렬해보기, 한번더
+SELECT ENAME, DEPTNO, SAL FROM EMP ORDER BY SAL DESC;
+-- 퀴즈3
+-- SALGRADE 테이블 에서 급여 등급(GRADE)을 오름차순
+-- 최고 급여(HISAL) 내림차순으로 정렬해보기
+SELECT * FROM SALGRADE ORDER BY GRADE ASC, HISAL DESC;
